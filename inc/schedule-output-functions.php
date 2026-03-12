4d7ab706bd25087c7ba0df1fdcd1f5f63f6a0515
Cory Birdsong <cory@birdsong.dev>
2026-03-12 10:44:11 -0500

    Remove deprecated PHP function

--- inc/schedule-output-functions.php
+++ inc/schedule-output-functions.php
@@ -369,7 +369,7 @@ function acfes_schedule_output( $props ) {
 
 				// If the next element in the table is the same as the current one, use colspan
 				if ( $key !== key( array_slice( $columns, -1, 1, true ) ) ) {
-					while ( $pair = each( $columns_clone ) ) {
+					foreach ($columns_clone as $pair) {
 						if ( $pair['key'] === $key ) {
 							continue;
 						}
